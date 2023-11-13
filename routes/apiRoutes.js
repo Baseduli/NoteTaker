2b74c584c4afff1b80a1c29bcdaa901634c32d8d
baseduli <ulisesvivar123@gmail.com>
2023-11-13 16:42:47 -0500

    Appears to be working now for adding notes. Can't seem to get the note to appear when I click on one of the already saved ones. Will have to double check my routes to confirm I have added the right functionality.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,30 +6,56 @@ const path = require('path');
 router.get('/notes', (req, res) => {
     fs.readFile(path.join(__dirname, '../db.json'), 'utf8', (err, data) => {
         if (err) {
-            console.log(err);
-            return;
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read notes' });
         }
         res.json(JSON.parse(data));
     });
 });
 
 router.post('/notes', (req, res) => {
-    fs.writeFile(path.join(__dirname, '../db.json'), JSON.stringify(req.body), (err) => {
+    fs.readFile(path.join(__dirname, '../db.json'), 'utf8', (err, data) => {
         if (err) {
-            console.log(err);
-            return;
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read notes' });
         }
-        res.json(req.body);
+        let notes;
+        try {
+            notes = JSON.parse(data);
+            if (!Array.isArray(notes)) {
+                notes = [];
+            }
+        } catch (parseErr) {
+            notes = [];
+        }
+        notes.push(req.body);
+
+        fs.writeFile(path.join(__dirname, '../db.json'), JSON.stringify(notes), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to save note' });
+            }
+            res.json(req.body);
+        });
     });
 });
 
 router.delete('/notes/:id', (req, res) => {
-    fs.delete(path.join(__dirname, '../db.json'), JSON.stringify(req.body), (err) => {
+    fs.readFile(path.join(__dirname, '../db.json'), 'utf8', (err, data) => {
         if (err) {
-            console.log(err);
-            return;
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read notes' });
         }
-        res.json(req.body);
+        const notes = JSON.parse(data);
+        const updatedNotes = notes.filter(note => note.id !== req.params.id);
+
+        fs.writeFile(path.join(__dirname, '../db.json'), JSON.stringify(updatedNotes), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to delete note' });
+            }
+            res.json({ message: 'Note deleted successfully' });
+        });
     });
 });
 
--- server.js
+++ server.js
@@ -7,11 +7,12 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+const apiRoutes = require("./routes/apiRoutes");
+app.use('/api', apiRoutes);
+
 const htmlRoutes = require("./routes/htmlRoutes");
 app.use('/', htmlRoutes);
 
-const apiRoutes = require("./routes/apiRoutes");
-app.use('/api', apiRoutes);
 
 
 app.listen(port, () => {
